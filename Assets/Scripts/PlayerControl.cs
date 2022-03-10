4a44cc96372b0c892e4889f1f9c2e6e6413ef259
Maldonado97 <90938489+Maldonado97@users.noreply.github.com>
2022-03-10 20:51:44 -0300

    Movimentação básica atualizada
    Giro agora depende da velocidade na qual o navio se encontra navegando, fazendo com que fique mais parecido com navios com propulsão mecânica tradicionais.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,38 +5,59 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private Rigidbody2D playerRB;
-    private float rigidBodySpeed;
-    public float speed;
-    public float turnSpeed;
+    public float forwardImpulse;
+    public float turnTorque;
+    private float shipHeading;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = gameObject.GetComponent<Rigidbody2D>();
-        //rigidBodySpeed = playerRB.velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
         MovePlayer();
+        GetShipHeading();
+        //Debug.Log(GetShipSpeed());
     }
     void MovePlayer()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            playerRB.AddRelativeForce(Vector2.up * speed * Time.deltaTime, ForceMode2D.Impulse);
+            playerRB.AddRelativeForce(Vector2.up * forwardImpulse * Time.deltaTime, ForceMode2D.Impulse);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            playerRB.AddTorque(-turnSpeed * Time.deltaTime, ForceMode2D.Impulse);
+            playerRB.AddTorque(-turnTorque * GetShipSpeed() * Time.deltaTime, ForceMode2D.Impulse);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            playerRB.AddTorque(turnSpeed * Time.deltaTime, ForceMode2D.Impulse);
+            playerRB.AddTorque(turnTorque * GetShipSpeed() * Time.deltaTime, ForceMode2D.Impulse);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            playerRB.AddRelativeForce(-Vector2.up * speed * Time.deltaTime, ForceMode2D.Impulse);
+            playerRB.AddRelativeForce(-Vector2.up * forwardImpulse * Time.deltaTime, ForceMode2D.Impulse);
         }
+    }
+    void GetShipHeading()
+    {
+        shipHeading = 360 - transform.eulerAngles.z;
+        //Debug.Log(Mathf.Sin(shipHeading));
+    }
+    float GetShipSpeed()
+    {
+        float forwardSpeed;
+        float xSpeed;
+        float ySpeed;
+        float headingRad;
+
+        xSpeed = playerRB.velocity.x;
+        ySpeed = playerRB.velocity.y;
+        headingRad = Mathf.Deg2Rad * shipHeading;
+        forwardSpeed = (xSpeed * Mathf.Sin(headingRad)) + (ySpeed * Mathf.Cos(headingRad));
+
+
+        return forwardSpeed;
     }
 }
